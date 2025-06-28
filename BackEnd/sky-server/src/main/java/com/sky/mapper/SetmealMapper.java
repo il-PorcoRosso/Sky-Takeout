03c156f224221bba31ee76d782c82a902a75e712
Clementine <tomoyo021126@outlook.com>
2025-06-28 16:21:07 +0800

    1、实现新增套餐功能 2、实现套餐分页查询功能

--- BackEnd/sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ BackEnd/sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,13 +1,23 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.SetmealVO;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface SetmealMapper {
+
+    @Insert("insert into setmeal(category_id, name, price, description, image, create_time, update_time, create_user, update_user)" +
+            "VALUES" +
+            "(#{categoryId}, #{name}, #{price}, #{description}, #{image}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Setmeal setmeal);
 
     /**
      * 根据分类id查询套餐的数量
@@ -23,4 +33,11 @@ public interface SetmealMapper {
      */
     @AutoFill(OperationType.UPDATE)
     void update(Setmeal setmeal);
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    Page<SetmealVO> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
